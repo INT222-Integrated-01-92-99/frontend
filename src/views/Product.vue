17d430aa3a8dbeb1291ecb312ccd8df74ee0a434
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-09-22 02:11:46 +0700

    [add] new input (In Stocks)

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,36 +1,34 @@
 <template>
-  <div class="product">
-    <div class="bg-cream-light bg-fixed">
-      <div
-        class="bg-gray-extra-light pt-20 lg:pr-20 pr-16 flex justify-end space-x-2"
+  <div class="product mb-5">
+    <div
+      class="bg-gray-extra-light pt-20 lg:pr-20 pr-16 flex justify-end space-x-2"
+    >
+      <base-button
+        @click="(viewAddChange = true), this.$router.push('/product/views')"
+        txtcolor="text-black"
+        txtbutt="View Products"
+        class="font-prompt-regular-400 focus:outline-none py-2 px-2 pt-4 text-base text-black hover:text-red-light transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 focus:text-red-light"
       >
-        <base-button
-          @click="(viewAddChange = true), this.$router.push('/product/views')"
-          txtcolor="text-black"
-          txtbutt="View Products"
-          class="focus:outline-none font-serif py-2 px-2 pt-4 text-base text-black hover:text-red-light transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 focus:text-red-light"
-        >
-        </base-button>
-        <p class="text-center text-black text-lg pt-3">
-          |
-        </p>
-        <base-button
-          @click="(viewAddChange = false), this.$router.push('/product/add')"
-          txtcolor="text-black"
-          txtbutt="Add Product"
-          class="focus:outline-none font-serif py-2 px-2 pt-4 text-base text-black hover:text-red-light transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 focus:text-red-light"
-        >
-        </base-button>
-      </div>
-      <div v-if="!viewAddChange">
-        <base-form :pro-id="proId"></base-form>
-      </div>
-      <div v-else>
-        <component
-          class="lg:flex-col"
-          v-bind:is="currentTabComponent"
-        ></component>
-      </div>
+      </base-button>
+      <p class="text-center text-black text-lg pt-3">
+        |
+      </p>
+      <base-button
+        @click="(viewAddChange = false), this.$router.push('/product/add')"
+        txtcolor="text-black"
+        txtbutt="Add Product"
+        class="font-prompt-regular-400 focus:outline-none py-2 px-2 pt-4 text-base text-black hover:text-red-light transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 focus:text-red-light"
+      >
+      </base-button>
+    </div>
+    <div v-if="!viewAddChange">
+      <base-form :pro-id="proId"></base-form>
+    </div>
+    <div v-else>
+      <component
+        class="lg:flex-col"
+        v-bind:is="currentTabComponent"
+      ></component>
     </div>
   </div>
 </template>
@@ -56,10 +54,11 @@ export default {
           brand: addNewPro.brandName,
           proName: addNewPro.proName,
           proDescript: addNewPro.proDescription,
+          proAmount: addNewPro.proAmount,
           proPrice: addNewPro.proPrice,
           proMfd: addNewPro.proMFDDATE,
           proPathImg: addNewPro.proPathImg,
-          item: addNewPro.proCol,
+          prowithcolor: addNewPro.proCol,
         });
         const blob = await new Blob([jsonPro], {
           type: "application/json",
