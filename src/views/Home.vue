0cc4aa316003d3dd92023e74164b391569c32f3b
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-09-22 02:08:26 +0700

    [add] redesign page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="mx-auto w-full h-screen bg-cover" style="background-image: url('https://theklosetedit.com/wp-content/uploads/2019/09/flaunter-com-LI_8-X9cei0-unsplash-4000x2000.jpg');">
+  <div
+    class="mx-auto w-full h-screen bg-cover"
+    style="background-image: url('https://theklosetedit.com/wp-content/uploads/2019/09/flaunter-com-LI_8-X9cei0-unsplash-4000x2000.jpg');"
+  >
     <div>
-      <div
-        class="2xl:pt-80 lg:pt-56 pt-40 text-center"
-      >
+      <div class="2xl:pt-80 lg:pt-56 pt-40 text-center">
         <!-- <img src="../assets/home-bg.png"> -->
         <div class="justify-center">
-          <router-link to="/product"
+          <router-link
+            to="/product"
             ><base-button
               bgcolor="bg-red-light"
               txtcolor="text-white"
               txtbutt="SHOP NOW!"
-              class="focus:outline-none hover:bg-red-dark hover:text-white font-serif font-semibold transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 p-2 rounded-sm 2xl:text-3xl lg:text-sm text-xs 2xl:w-1/5 2xl:h-16 lg:w-28 lg:h-10 w-24 h-9"
+              class="font-rozha focus:outline-none hover:bg-red-dark hover:text-white tracking-widest font-semibold transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 p-2 rounded-sm 2xl:text-4xl lg:text-sm text-xs 2xl:w-1/5 2xl:h-16 lg:w-28 lg:h-10 w-24 h-9"
             >
             </base-button
           ></router-link>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,66 +1,61 @@
 <template>
   <div class="team">
-    <div class="bg-cream-light h-screen">
-      <div class="pb-8 pt-28 flex justify-center space-y-4 ">
-        <h1 class="font-serif text-cream-dark 2xl:text-5xl text-4xl font-bold">
+    <div class="h-screen">
+      <div class="pb-10 pt-28 flex justify-center space-y-4">
+        <h1 class="font-prompt-regular-400 font-semibold 2xl:text-5xl text-4xl">
           Our Team
         </h1>
       </div>
 
       <div class="flex justify-center 2xl:space-x-24 lg:space-x-14 space-x-10">
-        <base-card
-          class="2xl:w-1/5 w-1/4"
-        >
+        <base-card class="2xl:w-1/4 w-1/4">
           <div class="justify-col">
             <div class="h-full w-full mx-auto">
               <img class="rounded-t-sm" src="../assets/teamPic/fluke.png" />
-              <div class="mt-3 ml-4">
-                <p class="font-bold lg:text-lg text-sm">62130500001</p>
-                <p class="font-bold lg:text-lg text-sm">Kanawat Mamut</p>
+              <div class="mt-3">
+                <p class="font-prompt-regular-400 font-bold lg:text-lg text-sm">
+                  62130500001 - Kanawat Mamut
+                </p>
                 <p
-                  class="font-serif font-medium text-red-light lg:text-base text-sm pt-2"
+                  class="font-prompt-regular-400 text-red-light lg:text-base text-sm pt-2"
                 >
-                  Backend<br />Database<br />Network
+                  Backend, Database, Network
                 </p>
               </div>
             </div>
           </div>
         </base-card>
 
-        <base-card
-          class="2xl:w-1/5 w-1/4"
-        >
+        <base-card class="2xl:w-1/4 w-1/4">
           <div class="justify-col">
             <div class="h-full w-full mx-auto">
               <img class="rounded-t-sm" src="../assets/teamPic/friend.png" />
-              <div class="mt-3 ml-4 mb-3">
-                <p class="font-bold lg:text-lg text-sm">62130500092</p>
-                <p class="font-bold lg:text-lg text-sm">Samita Pinijsupol</p>
+              <div class="mt-3">
+                <p class="font-prompt-regular-400 font-bold lg:text-lg text-sm">
+                  62130500092 - Samita Pinijsupol
+                </p>
                 <p
-                  class="font-serif font-medium text-red-light lg:text-base text-sm pt-2"
+                  class="font-prompt-regular-400 text-red-light lg:text-base text-sm pt-2"
                 >
-                  Front-end Developer<br />Database<br />Devops<br />Network
+                  Front-end Developer, Database, Devops, Network
                 </p>
               </div>
             </div>
           </div>
         </base-card>
 
-        <base-card
-          class="2xl:w-1/5 w-1/4"
-        >
+        <base-card class="2xl:w-1/4 w-1/4">
           <div class="justify-col">
             <div class="h-full w-full mx-auto">
               <img class="rounded-t-sm" src="../assets/teamPic/best.png" />
-              <div class="mt-3 ml-4">
-                <p class="font-bold lg:text-lg text-sm">62130500099</p>
-                <p class="font-bold lg:text-lg text-sm">
-                  Supichaya Kanaraksanti
+              <div class="mt-3">
+                <p class="font-prompt-regular-400 font-bold lg:text-lg text-sm">
+                  62130500099 - Supichaya Kanaraksanti
                 </p>
                 <p
-                  class="font-serif font-medium text-red-light lg:text-base text-sm pt-2"
+                  class="font-prompt-regular-400 text-red-light lg:text-base text-sm pt-2"
                 >
-                  Front-end Developer<br />Database<br />Devops<br />Network
+                  Front-end Developer, Database, Devops, Network
                 </p>
               </div>
             </div>
@@ -74,16 +69,7 @@
 <script>
 export default {
   data() {
-    return {
-      theme: "blue-theme",
-    };
+    return {};
   },
 };
 </script>
-
-<style>
-.blue-theme {
-  color: navy;
-  background: white;
-}
-</style>
