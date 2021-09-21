3e07b73849301b8d743733c116f4a5e1e96be226
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-09-22 02:01:17 +0700

    [add] format doc

--- src/components/BaseDelete.vue
+++ src/components/BaseDelete.vue
@@ -1,12 +1,12 @@
 <script>
 export default {
-    props:["send-to-delete","send-id"],
-data() {
+  props: ["send-to-delete", "send-id"],
+  data() {
     return {
       urlProduct: "http://localhost:3000/delete",
     };
   },
-methods: {
+  methods: {
     async fetch(url) {
       const res = await fetch(url);
       const data = await res.json(url);
@@ -23,22 +23,20 @@ methods: {
         } catch (error) {
           console.log(`This product could not delete because ${error}`);
         }
-        this.$emit("will-set-false", this.sendId)
+        this.$emit("will-set-false", this.sendId);
         alert("Your product is now delete");
-        this.$router.push("/product/views")
+        this.$router.push("/product/views");
       } else {
         alert("Your delete was cancel.");
       }
     },
   },
-   watch: {
+  watch: {
     sendId: function check() {
-      if(this.sendToDelete == true){
-          this.deletePro(this.sendId);
-          
+      if (this.sendToDelete == true) {
+        this.deletePro(this.sendId);
       }
-      
     },
   },
-}
+};
 </script>
