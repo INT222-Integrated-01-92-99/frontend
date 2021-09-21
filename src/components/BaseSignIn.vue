e9bea2afd4cb0df20ebd57c3f5f1ac1ba9c06eee
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-09-22 02:10:03 +0700

    [add] password show/hide

--- src/components/BaseSignIn.vue
+++ src/components/BaseSignIn.vue
@@ -14,12 +14,18 @@
           ></div>
           <div class="flex flex-col w-full md:w-1/2 p-4">
             <div class="flex flex-col flex-1 justify-center mb-8">
-              <h1 class="text-4xl text-center font-thin">Sign In</h1>
+              <h1
+                class="font-prompt-regular-400 font-bold text-4xl text-center"
+              >
+                Sign In
+              </h1>
               <div class="w-full mt-4">
                 <form class="form-horizontal w-3/4 mx-auto">
                   <div class="flex -mx-3">
                     <div class="w-full px-3 mb-5">
-                      <label for="" class="text-xs font-semibold px-1"
+                      <label
+                        for=""
+                        class="font-prompt-regular-400 text-base font-medium px-1"
                         >Username</label
                       >
                       <div class="flex">
@@ -32,7 +38,7 @@
                         </div>
                         <input
                           type="email"
-                          class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                          class="font-prompt-regular-400 w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                           placeholder="Username"
                         />
                       </div>
@@ -40,9 +46,12 @@
                   </div>
                   <div class="flex -mx-3">
                     <div class="w-full px-3 mb-5">
-                      <label for="" class="text-xs font-semibold px-1"
+                      <label
+                        for=""
+                        class="font-prompt-regular-400 text-base font-medium px-1"
                         >Password</label
                       >
+
                       <div class="flex">
                         <div
                           class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
@@ -52,22 +61,28 @@
                           ></i>
                         </div>
                         <input
-                          type="email"
-                          class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                          :type="[showPassword ? 'text' : 'password']"
+                          class="font-prompt-regular-400 w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                           placeholder="Password"
                         />
+                        <button
+                          class="block"
+                          @click.prevent="showPassword = !showPassword"
+                        >
+                          1
+                        </button>
                       </div>
                     </div>
                   </div>
                   <div
-                    class="cursor-pointer text-right text-red-light hover:text-red-dark underline"
+                    class="font-prompt-regular-400 cursor-pointer text-right text-red-light hover:text-red-dark underline"
                   >
                     Forgot Your Password?
                   </div>
                   <div class="flex flex-col mt-8">
                     <button
                       type="submit"
-                      class="bg-red-light hover:bg-red-dark text-white text-sm font-semibold py-2 px-4 rounded"
+                      class="font-prompt-regular-400 bg-red-light hover:bg-red-dark text-white text-base font-medium py-2 px-4 rounded"
                     >
                       Sign In
                     </button>
@@ -75,7 +90,7 @@
                 </form>
                 <div class="text-center mt-4">
                   <a
-                    class="no-underline hover:underline text-blue-dark text-xs"
+                    class="no-underline hover:underline font-prompt-regular-400 text-sm"
                     href="{{ route('password.request') }}"
                   >
                     Don't have an account? Sign Up
@@ -87,39 +102,22 @@
         </div>
       </div>
     </div>
-    <!-- <div class="flex flex-col">
-      <h1 class="">Sign In</h1>
-
-      <div class="flex flex-col">
-        <label class="font-serif lg:text-lg text-base">Username </label>
-        <input
-          class="
-                  input
-                  font-serif
-                  w-full
-                  mt-1
-                  py-1
-                  px-4
-                  rounded-sm
-                  bg-white
-                  dark:bg-gray-800
-                  border border-gray-400
-                  dark:border-gray-700
-                  text-gray-800
-                  focus:border-indigo-500
-                  focus:outline-none
-                "
-          :class="{ 'bg-red-50': UserInput }"
-          id="userName"
-          type="text"
-          size="32"
-          placeholder="Your username"
-          v-model.trim="enterUser"
-        />
-      </div>
-      <p v-if="UserInput" class="font-serif text-red-600">
-        Please enter your Username !
-      </p>
-    </div> -->
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      showPassword: false,
+      // password: "",
+      // passwordFieldType: "password",
+    };
+  },
+  methods: {
+    // switchVisibility() {
+    //   this.passwordFieldType =
+    //     this.passwordFieldType === "password" ? "text" : "password";
+    // },
+  },
+};
+</script>
