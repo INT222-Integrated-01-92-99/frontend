6e47d5148d51c2b38c497148db506108e04afa02
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-09-22 02:12:28 +0700

    [add] back button

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="detail bg-cream-light h-screen">
-    <div class="">
-      <base-detail-product></base-detail-product>
+    <div class="bg-gray-extra-light pt-20">
+      <button
+        @click="this.$router.push('/product/views')"
+        class="2xl:w-20 2xl:h-9 w-10 h-7 mt-3 ml-24 flex justify-center transition ease-in duration-300 transform hover:-translate-x-6 active:translate-x-0"
+      >
+        <img class="h-2/3" src="../assets/icon/arrow-back.svg" />
+        <span class="font-prompt-regular-400">Back</span>
+      </button>
     </div>
+    <base-detail-product></base-detail-product>
   </div>
 </template>
 
@@ -11,7 +18,9 @@ import BaseDetailProduct from "../components/BaseDetailProduct.vue";
 export default {
   components: { BaseDetailProduct },
   data() {
-    return {};
+    return {
+      routes: "/product/views",
+    };
   },
 };
 </script>
