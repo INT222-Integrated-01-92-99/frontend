aaaab6633b58857ca250b7882f9d5f9a8b6399b7
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-09-22 18:49:01 +0700

    [edit] responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,7 @@
     style="background-image: url('https://theklosetedit.com/wp-content/uploads/2019/09/flaunter-com-LI_8-X9cei0-unsplash-4000x2000.jpg');"
   >
     <div>
-      <div class="2xl:pt-80 lg:pt-56 pt-40 text-center">
-        <!-- <img src="../assets/home-bg.png"> -->
+      <div class="2xl:pt-80 pt-96 text-center">
         <div class="justify-center">
           <router-link
             to="/product"
@@ -13,7 +12,7 @@
               bgcolor="bg-red-light"
               txtcolor="text-white"
               txtbutt="SHOP NOW!"
-              class="font-rozha focus:outline-none hover:bg-red-dark hover:text-white tracking-widest font-semibold transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 p-2 rounded-sm 2xl:text-4xl lg:text-sm text-xs 2xl:w-1/5 2xl:h-16 lg:w-28 lg:h-10 w-24 h-9"
+              class="font-rozha focus:outline-none hover:bg-red-dark hover:text-white tracking-widest font-semibold transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 p-2 rounded-sm 2xl:text-4xl lg:text-3xl text-2xl 2xl:w-1/5 lg:1/5 md:w-1/4 "
             >
             </base-button
           ></router-link>
