6bb8a89b7e81041b3c0e8e447bf7698357075298
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-10-18 01:17:30 +0700

    [edit] doing receipt page

--- src/views/Receipt.vue
+++ src/views/Receipt.vue
@@ -4,15 +4,39 @@
       <button
         @click="this.$router.push('/product/views')"
         :class="{ 'text-red-light underline': routes == '/product/views' }"
-        class="2xl:w-20 2xl:h-9 w-10 h-7 mt-3 ml-24 flex transition ease-in duration-300 transform hover:-translate-x-6 active:translate-y-0"
+        class="
+          2xl:w-20 2xl:h-9
+          w-10
+          h-7
+          mt-3
+          ml-24
+          flex
+          transition
+          ease-in
+          duration-300
+          transform
+          hover:-translate-x-6
+          active:translate-y-0
+        "
       >
         <img class="h-2/3" src="../assets/icon/arrow-back.svg" />
         <span class="font-prompt-regular-400">Back</span>
       </button>
     </div>
-    <div class="flex justify-center my-6">
+    <div class="flex justify-center my-6" v-for="r in receipt"
+              :key="r.idReceipt"
+             >
       <div
-        class="flex flex-col w-full p-8 text-gray-800 border-2 border-gray-500 pin-r pin-y md:w-4/5 lg:w-4/5"
+        class="
+          flex flex-col
+          w-full
+          p-8
+          text-gray-800
+          border-2 border-gray-500
+          pin-r pin-y
+          md:w-4/5
+          lg:w-4/5
+        "
       >
         <div class="flex-1">
           <table class="w-full text-sm lg:text-base" cellspacing="0">
@@ -28,53 +52,26 @@
                 <th class="text-center">Total price</th>
               </tr>
             </thead>
-            <tbody>
-              <tr
-                v-for="pc in cart.cartDetails"
-                :key="pc.idCartDetail"
-                class="border-b-2"
-              >
-                <td class="pb-4 md:table-cell">
-                  <img
-                    :src="urlImage + '/' + pc.product.proPathImg"
-                    class="w-20"
-                  />
-                </td>
+
+            <tbody
+              
+            >
+              <tr v-for="rd in r.receiptDetailsList" :key="rd.idReceiptDetails">
                 <td class="w-1/4">
-                  <p class="mt-5 mb-2 md:ml-4">{{ pc.product.proName }}</p>
-                  <p class="mb-2 md:ml-4">{{ pc.product.brand.brandName }}</p>
-                  <div
-                    class="mb-2 md:ml-4 inline-flex font-prompt-regular-400 font-medium"
-                  >
-                    Color: &nbsp;
-                    <div
-                      class="
-                      w-6
-                      h-6
-                      rounded-sm
-                      border
-                      "
-                      v-bind:style="{ backgroundColor: pc.color.colorCode }"
-                    ></div>
-                  </div>
+                  <p class="mt-5 mb-2 md:ml-4">{{ rd.proName }}</p>
+                  <p class="mb-2 md:ml-4">{{ rd.brandName }}</p>
                 </td>
                 <td class="justify-center md:justify-end md:flex mt-6">
                   <div class="mt-14">
                     <div class="relative flex flex-row w-1/2 h-8">
-                      <input
-                        type="number"
-                        min="0"
-                        v-model="pc.proPerPiece"
-                        @change="persist(pc)"
-                        class="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black"
-                      />
+                      {{ rd.proPerPiece }}
                     </div>
                   </div>
                 </td>
                 <td class="hidden text-center md:table-cell">
                   <span class="text-sm lg:text-base font-medium">
                     {{
-                      pc.product.proPrice
+                      rd.proPrice
                         .toString()
                         .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
                     }}
@@ -84,7 +81,7 @@
                 <td class="text-center">
                   <span class="text-sm lg:text-base font-medium">
                     {{
-                      (pc.proPerPiece * pc.product.proPrice)
+                      (rd.proPerPiece * rd.proPrice)
                         .toString()
                         .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
                     }}
@@ -99,4 +96,25 @@
     </div>
   </div>
 </template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      receipt: [],
+    };
+  },
+  methods: {
+    async fetch(url) {
+      const res = await fetch(url);
+      const data = await res.json(url);
+      return data;
+    },
+  },
+  async created() {
+    this.receipt = await this.fetch("http://localhost:3000/receipt");
+    console.log(this.receipt);
+  },
+};
+</script>
 
