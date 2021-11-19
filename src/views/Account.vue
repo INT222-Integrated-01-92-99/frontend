137b977ec3708b4e9d86c86f627f8cd03415f045
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-11-19 18:26:13 +0700

    [delete] search bar and check box

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -73,44 +73,6 @@
       </h1>
     </div>
     <!-- component -->
-    <div class="flex justify-center 2xl:w-1/3 lg:w-1/3 w-1/2 relative mx-auto">
-      <input
-        class="
-          font-prompt-regular-400
-          border-b-2 border-gray-400
-          bg-white
-          h-10
-          w-full
-          px-1
-          pr-16
-          text-sm
-          focus:border-gray-dark focus:outline-none
-        "
-        type="search"
-        name="search"
-        placeholder="Search"
-      />
-      <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
-        <svg
-          class="text-gray-600 h-4 w-4 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          version="1.1"
-          id="Capa_1"
-          x="0px"
-          y="0px"
-          viewBox="0 0 56.966 56.966"
-          style="enable-background: new 0 0 56.966 56.966"
-          xml:space="preserve"
-          width="512px"
-          height="512px"
-        >
-          <path
-            d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
-          />
-        </svg>
-      </button>
-    </div>
     <div class="w-11/12 mx-auto">
       <div class="mt-10 overflow-x-auto">
         <div class="flex items-center justify-center font-sans overflow-hidden">
@@ -355,9 +317,6 @@
                       leading-normal
                     "
                   >
-                    <th class="border-r p-2">
-                      <input type="checkbox" />
-                    </th>
                     <th class="py-3 px-6 text-left">USER ID</th>
                     <th class="py-3 px-6 text-left">USERNAME</th>
                     <th class="py-3 px-6 text-left">PASSWORD</th>
@@ -375,11 +334,6 @@
                     :key="r.idAccount"
                     class="border-b border-gray-200 hover:bg-gray-100"
                   >
-                    <td class="py-3 px-6 text-left whitespace-normal">
-                      <div class="flex items-center">
-                        <input type="checkbox" />
-                      </div>
-                    </td>
                     <td class="py-3 px-6 text-left whitespace-normal">
                       <div class="flex items-center">
                         <span class="font-medium">{{ r.idAccount }}</span>
