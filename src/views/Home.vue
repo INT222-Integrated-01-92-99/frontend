2973c8cd92e673ed3d5d96211ddbb1ad3549bf09
Supichaya <supichaya.jb@mail.kmutt.ac.th>
2021-11-22 15:46:20 +0700

    [edit] guest can see SHOP NOW!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,6 +19,23 @@
         </div>
       </div>
     </div>
+    <div v-else>
+      <div class="2xl:pt-80 lg:pt-96 md:pt-44 pt-96 text-center">
+        <div class="justify-center">
+          <router-link
+            to="/product"
+            ><base-button
+              bgcolor="bg-red-light"
+              txtcolor="text-white"
+              txtbutt="SHOP NOW!"
+              class="font-rozha focus:outline-none hover:bg-red-dark hover:text-white tracking-widest font-semibold transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 p-2 rounded-sm 2xl:text-4xl lg:text-3xl md:text-3xl text-2xl 2xl:w-1/5 lg:1/5 md:w-1/3 "
+            >
+            </base-button
+          ></router-link>
+        </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
